8b4c053027a22369c01c1a097590dd77b0578c7a
CHANDAN9339 <deyc6053@gmail.com>
2025-05-19 04:08:34 +0000

    Data Type

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict";// means treat all the JS code as newer version
+
+//alert( 3 + 3)  // it will give us error beacause we are using node js ,not browser
+
+// you have to notice that code readability should be high 
+
+
+let name = "Chandan"
+let age = 26
+let isLoggedIn = true 
+
+
+
+
+// TYPE OF DATATYPES
+
+// number = 2^53   //(Range)
+// bigint
+// string = prefere to use ""
+
+// boolean = true/false
+// null = standalone value 
+// undefined = when value did not assigne 
+// symbol = for finding uniqueness 
+
+
+// object
+
+console.log(typeof "Chandan"); // we use 'typeof' function to find what type of data is taken here like string , number etc etc
+console.log(typeof null)       // output will be object 
+console.log(typeof undefined)  // output will be undefined beacause this is a type in JS 
